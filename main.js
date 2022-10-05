5cbb2871cbbe668a7a17e28a31209f76556d1e0b
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2022-10-05 13:26:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ let close0 = document.getElementById('close');
 var menu = document.getElementById('menu');
 var ul = document.getElementById('ul');
 var ct = document.getElementById('ct');
-
+var nav1 = document.getElementById('nav');
+var logo = document.getElementById('logo');
 
 
 open0.addEventListener('click', function() {
     close0.style.display = 'block';
     open0.style.display = 'none';
     menu.style.width = '100%';
+    menu.style.borderRadius = '0';
     menu.style.height = '90vh';
     menu.style.top = '10vh';
     menu.style.right = '0';
@@ -22,13 +24,23 @@ open0.addEventListener('click', function() {
 close0.addEventListener('click', function() {
     open0.style.display = 'block';
     close0.style.display = 'none';
+    menu.style.borderRadius = '100%';
     menu.style.width = '0px';
     menu.style.height = '0px';
-    menu.style.top = '10px';
-    menu.style.right = '10px';
+    menu.style.top = '5vh';
+    menu.style.right = '5vw';
+    menu.style.position = 'fixed';
     menu.style.transition = '.6s'
     ul.style.display = 'none';
     ct.style.display = 'none';
     ul.style.transition = '.6s';
     ct.style.transition = '.6s';
 });
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+        nav1.style.background = 'rgba(0, 0, 0, .2)';
+    } else {
+        nav1.style.background = 'rgba(0, 0, 0, .0)';
+    }
+})
